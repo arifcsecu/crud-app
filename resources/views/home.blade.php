29859562c3b1df0f9b9b6d0f7405b53367f45610
Ariful Islam <arifcsecu16@gmail.com>
2025-03-16 15:44:34 +0600

    home page done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div style="border: 3px solid black;">
+        <h2>Register</h2>
+        <form action="/register">
+            <input type="text" placeholder="Name">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <button>Register</button>
+        </form>
+
+    </div>
+    
+</body>
+</html>
+
+
